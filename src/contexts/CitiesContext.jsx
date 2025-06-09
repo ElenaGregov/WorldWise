69b794c7eb6d2441f5c4df286d894cbcc7e57ff5
Elena Gregov <124687938+ElenaGregov@users.noreply.github.com>
2025-06-09 14:27:41 +0200

    Update CitiesContext.jsx API

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -7,7 +7,7 @@ import {
 } from "react";
 import PropTypes from "prop-types";
 
-const BASE_URL = "http://localhost:9000";
+const BASE_URL = "https://worldwise-server-fjv2.onrender.com/cities";
 
 const CitiesContext = createContext();
 
